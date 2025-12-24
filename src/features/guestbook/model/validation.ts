75467f6b2a11ba5805f6a38cee391d3b24c711b2
hongsoohyuk <hongsoohyuk1120@gmail.com>
2025-12-24 15:51:39 +0900

    refactor: pagination

--- src/features/guestbook/model/validation.ts
+++ src/features/guestbook/model/validation.ts
@@ -1,15 +1,15 @@
-import { z } from 'zod';
+import {z} from 'zod';
 
 export const schema = z.object({
   author_name: z
     .string()
     .trim()
-    .nonempty({message: 'formSection.nameRequired'})
-    .max(12, {message: 'formSection.nameTooLong'}),
+    .min(1, {message: 'Common.validation.required'})
+    .max(12, {message: 'Common.validation.maxLength'}),
   message: z
     .string()
     .trim()
-    .nonempty({message: 'formSection.messageRequired'})
-    .max(40, {message: 'formSection.messageTooLong'}),
-  emotions: z.array(z.string()).max(2, {message: 'formSection.emotionLimit'}).default([]),
+    .min(1, {message: 'Common.validation.required'})
+    .max(40, {message: 'Common.validation.maxLength'}),
+  emotions: z.array(z.string()).max(2, {message: 'Guestbook.formSection.validation.emotionLimit'}).default([]),
 });
