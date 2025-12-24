409532494c66aedad12377dd5ef049ebc449460f
hongsoohyuk <hongsoohyuk1120@gmail.com>
2025-12-24 13:31:17 +0900

    refactor: guestbook page uses http module

--- src/entities/guestbook/index.ts
+++ src/entities/guestbook/index.ts
@@ -1,7 +1,7 @@
 export {normalizeGuestbookEmotions, type Emotion, type EmotionCode, type EmotionOption} from '../emotion/model/type';
 export {createGuestbookEntry} from './api/create-guestbook';
 export {QueryKeyFactory} from './api/guestbook.query';
-export {fetchGuestbookList} from './api/list-guestbook';
+export {fetchGuestbookList, fetchInitialGuestbook} from './api/list-guestbook';
 export {
   type CreateGuestbookEntryPayload,
   type GuestbookEntriesResponse,
