eb8360119db38758933fe273bff23cc31760908e
hongsoohyuk <hongsoohyuk1120@gmail.com>
2025-12-30 00:39:12 +0900

    chore:: guestbook layout 수정

--- src/shared/ui/layout/header.tsx
+++ src/shared/ui/layout/header.tsx
@@ -1,4 +1,4 @@
-import {LocaleSwitcher} from '@/shared/ui/locale-switcher';
+import {LocaleSwitch} from '@/shared/ui/locale-switch';
 import {ThemeSwitch} from '@/shared/ui/theme-switch';
 import {SITE_CONFIG} from '@/shared/config/site';
 import {HeaderNav} from './header-nav';
@@ -6,14 +6,11 @@ import {HeaderNav} from './header-nav';
 export async function Header() {
   return (
     <header className="sticky top-0 z-50 w-full border-b border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/40">
-      <div className="container mx-auto flex h-10 max-w-screen-2xl items-center">
+      <div className="container mx-auto flex h-12 max-w-screen-2xl items-center">
         <HeaderNav siteName={SITE_CONFIG.name} />
-
-        <div className="flex flex-1 items-center justify-between space-x-2 justify-end">
-          <nav className="flex items-center">
-            <LocaleSwitcher />
-            <ThemeSwitch />
-          </nav>
+        <div className="flex flex-1 items-center justify-end">
+          <LocaleSwitch />
+          <ThemeSwitch />
         </div>
       </div>
     </header>
